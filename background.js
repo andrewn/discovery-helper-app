89a105e5b01c32c9b36fdd280edba2545d0c9585
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-07-14 19:57:36 +0100

    Split TXT records into DNS-SD key-value pairs

--- background.js
+++ background.js
@@ -26,8 +26,40 @@ function handleServicesFound(error) {
     services = serviceFinder.instances();
   }
 
+  services = services.map(transformTxtToKeys);
+
   console.log('Found %o:', services.length, services);
   sendMessage(recipients, services);
+}
+
+/*
+  Parse a services TXT record values into 
+  key/value pairs on the `txt` object.
+  e.g.  service.txt = ['id=15']
+        => service.txt.id = 15
+
+  Also attempts to parse JSON
+  e.g.  service.txt = ['player={ id:15, name="dave"}']
+        => service.txt.player.id = 15
+           service.txt.player.name = "dave"
+*/
+function transformTxtToKeys(service) {
+  if (service.txt && service.txt.map) {
+    service.txt.map(function (txt) {
+      var parts = txt.split('='),
+          key   = parts[0],
+          value = parts[1] || true;
+
+      try {
+        value = JSON.parse(value);
+      } catch (e) {
+        // Value isn't JSON
+      }
+
+      service.txt[key] = value;
+    });
+  }
+  return service;
 }
 
 function sendMessage(recievers, message) {
